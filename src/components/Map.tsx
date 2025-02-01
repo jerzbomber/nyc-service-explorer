09da887c137a4bd213b62b1ad74ee6fedda3a799
Toby Jackson <toby@mainstay.com>
2025-01-31 23:21:38 -0600

    Load the Map only on client side

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,18 +1,6 @@
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet"; // Needed for Leaflet marker icon adjustment
-
-// Fix missing marker icon issue with Leaflet
-// see https://stackoverflow.com/questions/49441600/react-leaflet-marker-icon-not-showing
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-  iconUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
-});
+import { useEffect, useState } from "react";
 
 interface Props {
   data: {
@@ -26,13 +14,35 @@ interface Props {
 
 const Map = ({ data, mapCenter }: Props) => {
   const defaultZoom = 12;
+  const [L, setL] = useState<typeof import("leaflet") | null>(null);
+
+  useEffect(() => {
+    import("leaflet").then((leaflet) => {
+      setL(leaflet);
+
+      // Fix missing marker icon issue with Leaflet
+      delete leaflet.Icon.Default.prototype._getIconUrl;
+      leaflet.Icon.Default.mergeOptions({
+        iconRetinaUrl:
+          "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
+        iconUrl:
+          "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+        shadowUrl:
+          "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+      });
+    });
+  }, []);
 
   // Custom hook to update map center when the prop changes
   const SetMapCenter = () => {
     const map = useMap();
-    map.setView([mapCenter.lat, mapCenter.lng], defaultZoom);
+    useEffect(() => {
+      map.setView([mapCenter.lat, mapCenter.lng], defaultZoom);
+    }, [map]);
     return null;
   };
+
+  if (!L) return <p>Loading map...</p>;
 
   return (
     <div className="h-full md:h-[calc(100vh-15rem)]">
